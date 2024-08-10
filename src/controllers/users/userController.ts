b2d09bf4c23c937094986823e519fdcdc0071e05
elcodee <ramdit21@gmail.com>
2024-08-10 19:34:19 +0700

    ✨ feat(backend): Order Chat
    - Add rating function

--- src/controllers/users/userController.ts
+++ src/controllers/users/userController.ts
@@ -1,6 +1,5 @@
 import { NextFunction, Request, Response } from "express";
 import { supabase } from "../../config/supabase";
-import { getCity, getProvince, getState } from "../location/location";
 import { responseBuilder } from "../../utils/helper";
 
 export const getAllUsers = async (req: Request, res: Response) => {
@@ -16,20 +15,8 @@ export const getAllUsers = async (req: Request, res: Response) => {
 export const getUserByID = async (req: Request, res: Response) => {
     try {
         if (req.params.id) {
-            const response: any = await supabase.from('users').select('id, username, email, phone_number, profile_pict, address, role, description, province, state, city, created_at').eq('id', req.params.id).order('id', { ascending: true });
-            const province: any = await getProvince();
-            const state: any = await getState(response.data[0]?.province);
-            const city: any = await getCity(response.data[0]?.state);
-
-            responseBuilder(res, {
-                ...response,
-                data: {
-                    ...response.data[0],
-                    province: province?.filter((el: any) => el.id === response.data[0]?.province)[0]?.name,
-                    state: state?.filter((el: any) => el.id === response.data[0]?.state)[0]?.name,
-                    city: city?.filter((el: any) => el.id === response.data[0]?.city)[0]?.name,
-                }
-            }, response.status);
+            const response: any = await supabase.from('users').select('id, username, is_approve, email, phone_number, profile_pict, address, role, description, created_at').order('id', { ascending: true });
+            responseBuilder(res, { data: response.data.filter((e: any) => e.id === +req.params.id)[0] }, response.status);
         } else {
             res.status(500).send({
                 message: "Params Required"
@@ -43,7 +30,7 @@ export const getUserByID = async (req: Request, res: Response) => {
 export const getUserByRole = async (req: Request, res: Response) => {
     try {
         if (req.params.role) {
-            const response: any = await supabase.from('users').select('id, username, email, phone_number, profile_pict, address, role, description, province, state, city, created_at').eq('role', req.params.role).order('id', { ascending: true });
+            const response: any = await supabase.from('users').select('id, username, is_approve, email, phone_number, profile_pict, address, role, description, province, state, city, created_at').eq('role', req.params.role).order('id', { ascending: true });
 
             responseBuilder(res, response, response.status);
         } else {
@@ -59,7 +46,7 @@ export const getUserByRole = async (req: Request, res: Response) => {
 export const updateUserProfile = async (req: any, res: Response) => {
     try {
         if (req.params.id) {
-            const response: any = await supabase.from('users').update({ username: req.body.username, description: req.body.description, address: req.body.address, phone_number: req.body.phone_number, is_approve: req.body.is_approve }).eq('id', req.params.id).select('id, username, email, phone_number, profile_pict, address, role, description, province, state, city, created_at')
+            const response: any = await supabase.from('users').update({ username: req.body.username, description: req.body.description, address: req.body.address, phone_number: req.body.phone_number }).eq('id', req.params.id).select('id, username, is_approve, email, phone_number, profile_pict, address, role, description, province, state, city, created_at')
 
             responseBuilder(res, { ...response, data: response.data[0] }, response.status);
         } else {
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -8,7 +8,7 @@ import { middleware } from '../controllers/auth/middlewareController';
 import { userLogout } from '../controllers/auth/logoutController';
 import { getCurrentLocation } from '../controllers/geolocation/geolocationController';
 import { addService, deleteService, getAllService, getServiceByCategory, getServiceByID, getServiceByMerchant, updateService } from '../controllers/services/serviceController';
-import { clientCreateNewChat, clientGetChatByID, getAllChatByChatID, getAllMessageByChatID, merchantGetChatByID, messagePlaceOrder, sendNewMessage } from '../controllers/chat/chatController';
+import { addRating, clientCreateNewChat, clientGetChatByID, clientGetOrderByID, getAllChatByChatID, getAllMessageByChatID, merchantGetChatByID, messagePlaceOrder, sendNewMessage } from '../controllers/chat/chatController';
 import { uploadAvatar, uploadService } from '../controllers/upload/uploadController';
 
 // Create a new Router instance
@@ -63,7 +63,9 @@ router.post('/new-message', sendNewMessage);
 router.post('/new-order-message/:id', messagePlaceOrder);
 router.get('/chat-parent-history/:id', getAllChatByChatID);
 router.get('/chat-history/:id', getAllMessageByChatID);
-
 router.get('/merchant-all-chat/:id', merchantGetChatByID);
+
+router.get('/client-all-order/:id', clientGetOrderByID);
+router.post('/client-add-rating/:id', addRating);
 
 export default router;
